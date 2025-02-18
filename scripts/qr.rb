ff01ddcc2690ab6957b64fd34d3d0fb076f16166
Tpj-root <trichy_hackerspace@outlook.com>
2025-02-18 16:40:28 +0530

    And Here We Go

--- scripts/qr.rb
+++ scripts/qr.rb
@@ -3,7 +3,14 @@
 # Describe : This code to generate a QR code with the user ID, making it easy to scan and run the simulation.
 # Usage : run qr <user_id>
 # 
-require 'rqrcode'
+require 'rubygems'
+begin
+  require 'rqrcode'
+rescue LoadError
+  puts "rqrcode gem not found. Installing..."
+  system('sudo gem install rqrcode')
+  require 'rqrcode'
+end
 
 class Qr < Sandbox::Script
   def main
@@ -17,17 +24,11 @@ class Qr < Sandbox::Script
     simlink = Trickster::Hackers::SimLink.new(id)
     url = simlink.generate
 
-    # Create a QR code from the URL
-    qrcode = RQRCode::QRCode.new(url.to_s)  # Ensure the URL is converted to a string
-
-    # You can customize the size and other options if needed
+    qrcode = RQRCode::QRCode.new(url.to_s)
     png = qrcode.as_png(size: 480)
 
-    # Generate the output filename based on the input player ID
     output_filename = "qrcode_#{id}.png"
-
-    # Save or display the QR code with the dynamically generated filename
-    File.open(output_filename, 'w') { |f| f.write(png.to_s) }
+    File.open(output_filename, 'wb') { |f| f.write(png.to_s) }
 
     @logger.log("QR code generated and saved as #{output_filename}")
   end
